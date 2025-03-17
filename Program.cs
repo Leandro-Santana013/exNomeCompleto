8c828fef9191d849ae8402ce94a501daa02d77fc
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-16 23:16:50 -0300

    exnome

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string  nome, sobrenome;
+    Console.WriteLine("Digite seu nome: ");
+    nome = Console.ReadLine();
+    Console.WriteLine("Digite seu sobrenome: ");
+    sobrenome = Console.ReadLine();
+    Console.WriteLine($"Seu nome completo é: {nome} {sobrenome}");
