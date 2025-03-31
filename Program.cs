ff9f2e3e31c152a0a07d095f23d518dba0cb704c
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-30 23:34:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string  nome, sobrenome;
-    Console.WriteLine("Digite seu nome: ");
-    nome = Console.ReadLine();
-    Console.WriteLine("Digite seu sobrenome: ");
-    sobrenome = Console.ReadLine();
-    Console.WriteLine($"Seu nome completo é: {nome} {sobrenome}");
- Console.WriteLine($"Seu nome de catalogo é: {sobrenome.ToUpper()}, {nome.ToLower()}");
+Console.WriteLine("Digite seu nome: ");
+nome = Console.ReadLine()!;
+Console.WriteLine("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+Console.WriteLine($"Seu nome completo é: {nome} {sobrenome}");
+Console.WriteLine($"Seu nome de catalogo é: {sobrenome.ToUpper()}, {nome.ToLower()}");
